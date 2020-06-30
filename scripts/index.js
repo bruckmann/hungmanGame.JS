ea972295db564c30b1d1ef1cc049a275f694f01a
Joaquimlagos <64992814+Joaquimlagos@users.noreply.github.com>
2020-06-30 02:30:29 -0300

    concertoerromadrug

--- scripts/index.js
+++ scripts/index.js
@@ -1,19 +1,20 @@
 const btnTry = document.getElementById('btntry');
 let dashes = document.getElementById('dashes');
-let letraa = document.getElementById('letraa')
-const letraA = document.getElementById('a')
+let textWords = document.getElementById('textWord');
+const Aletter = document.getElementById('a');
 
+Aletter.addEventListener('click' , putLetter)
 const palavras = [
-    'Florianópolis',
-    'Curitiba',
-    'São paulo',
-    'Porto Alegre',
-    'Brasilia',
-    'Rio de Janeiro',
-    'Manaus',
-    'Recife',
-    'Belo Horizonte',
-    'Goias'
+    'FLORIANOPOLIS',
+    'CURITIBA',
+    'SAO PAULO',
+    'PORTO ALEGRE',
+    'BRASILIA',
+    'RIO DE JANEIRO',
+    'MANAUS',
+    'RECIFE',
+    'BELO HORIZONTE',
+    'goias'
 ]
 
 btnTry.addEventListener('click', dashed)
@@ -44,20 +45,23 @@ function dashed ()  {
             dashes.textContent += "-"
 
         } else {
-            dashes.innerHTML += " "
+            dashes.textContent += " "
         }
 
     }
 }
 
 
-function palavras(){
-    
-    const palavra = SortedWord.charAt(i)
-    
+function  putLetter (){
+    const Letter = SortedWord.charAt(i)
+
+    Letter 
+
     for (i=0 ; i <= letters; i++){
-    if (palavra == letraA.value){
-        letraa.innerHTML += "A"
+   
+        if (Letter == Aletter.value) {
+
+            textWords.textContent += "A"
     }
     }
 }
