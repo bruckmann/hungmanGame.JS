2a426a126e784463e36afcc20bed4fc468e1c58b
Joaquimlagos <64992814+Joaquimlagos@users.noreply.github.com>
2020-07-11 20:27:34 -0300

    attgrande

--- scripts/index.js
+++ scripts/index.js
@@ -1,67 +1,67 @@
-const btnTry = document.getElementById('btntry');
-let dashes = document.getElementById('dashes');
-let textWords = document.getElementById('textWord');
-const Aletter = document.getElementById('a');
-
-Aletter.addEventListener('click' , putLetter)
-
-const palavras = [
-    'FLORIANOPOLIS',
-    'CURITIBA',
-    'SAO PAULO',
-    'PORTO ALEGRE',
-    'BRASILIA',
-    'RIO DE JANEIRO',
-    'MANAUS',
-    'RECIFE',
-    'BELO HORIZONTE',
-    'goias'
-]
-
-btnTry.addEventListener('click', dashed)
-
-
-function SorteiaPalavra(array) {
-    const numeroSort = Math.floor(Math.random() * 10)
-    return array[numeroSort]
-}
-
-
-const SortedWord = SorteiaPalavra(palavras)
-
-const letters = SortedWord.length
-
-
-
-
-function dashed ()  {
-
-    for (i=0;i<=letters;i++) {
-
-        const letter = SortedWord.charAt(i) 
-
-        if (letter != " ") {
-
-            dashes.textContent += "-"
-
-        } else {
-            dashes.textContent += " "
+const words = [
+    {
+      "name": "Florianopolis",
+      "tip": "Cidade"
+    },
+    {
+      "name": "Santa Catarina",
+      "tip": "Estado"
+    },
+    {
+      "name": "Brasil",
+      "tip": "País"
+    },
+  ] 
+  
+  function startGame() {
+    let drawnNumber; 
+    let drawnWord; 
+   
+    drawnNumber = Math.floor(Math.random() * (3 - 0)) + 0;
+    drawnWord = words[drawnNumber].name
+  
+    console.log(drawnNumber)
+    returnTraces(drawnWord)
+  
+    function returnTraces(word) {
+      let traces = "";
+      let element = document.getElementById("word");
+  
+      for (i = 0;i < word.length;i++) {
+        element.innerHTML += `<div id='letter${i}'>${drawnWord.charAt(i)}</div>`;
+        document.getElementById(`letter${i}`).style.visibility = "hidden";
+  
+          if (word.charAt(i) != " ") {
+              traces += "-";
+          } else {
+              traces += " ";
+          }
+      }
+  
+      document.getElementById("result").innerHTML = traces;
+  }
+    
+    function matchLetter(event) {
+      const value = event.target.value;
+      const wordLength = drawnWord.length; 
+  
+      for (i = 0;i < wordLength;i++) {
+        let letter = document.getElementById(`letter${i}`).textContent;
+        let letterElement = document.getElementById(`letter${i}`);
+        
+        letter = letter.toUpperCase();
+  
+        if(value == letter){
+          letterElement.style.visibility = "visible";
         }
     }
-}
-
-
-function putLetter () {
-
-
-    for (i=0 ; i <= letters; i++){
-
-        const Letter = SortedWord.charAt(i)
-        
-        if (Letter == Aletter.value) {
-
-            textWords.textContent += "A"
-        
-        }
     }
-}
+    
+    let buttons = document.querySelectorAll("button[name=letter]");
+    
+    for(button of buttons) {
+      button.addEventListener("click", matchLetter)
+    }  
+  };
+  
+  
